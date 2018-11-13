1ad6f5126ab4702b28e740ee79a6adfc8475fa6a
labkey-eyounske <eyounske@labkey.com>
2018-11-12 16:50:00 -0800

    Copyright and license updates   -- Update for November 2018   -- Now scanning .tf and .tfvars files (Terraform)   -- Deliberately leaving out SNPRC module (moving to Git very soon)

--- virtualbox/development/DEV/mozilla/firefox/vagrant.default/prefs.js
+++ virtualbox/development/DEV/mozilla/firefox/vagrant.default/prefs.js
@@ -1,1 +1,6 @@
+/*
+ * Copyright (c) 2017-2018 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 user_pref("browser.startup.homepage", "http://localhost:8080/labkey/home/project-begin.view?");
