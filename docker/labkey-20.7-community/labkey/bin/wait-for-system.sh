6b1e2cd388e62cd5dc3c45e2f7c7cc877bf03c53
labkey-jeckels <jeckels@labkey.com>
2025-01-03 10:57:33 -0800

    Delete scripts that are now totally defunct with switch to embedded Tomcat

--- docker/labkey-20.7-community/labkey/bin/wait-for-system.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Wait for the postgres container to be ready before starting LabKey.
-
-#Wait for the port
-wait-for-it.sh $DATABASE_HOST:$DATABASE_PORT -t 0
-
-echo "Starting LabKey Application"
-exec "$@"
